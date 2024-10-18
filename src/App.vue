46ca77355c12c0f545c8a7651cbecf906d2d4bce
coolleave <cpy2855048477@163.com>
2024-10-18 18:05:35 +0800

    element-plus按需导入

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,26 @@
 <script setup>
-import axios from 'axios';
-import {onMounted} from 'vue'
-import {ref} from 'vue'
-import Edit from './components/Edit.vue'
-
-// TODO: 列表渲染
-
-const list = ref([])
-
-// 定义查询列表函数
-const getList = async()=>{
- 
-  const res = await axios.get('/list')
-  list.value = res.data
-  
-  
-}
 
 
-// 使用挂载函数
-onMounted (()=>getList())
-
-
-// TODO: 删除功能
-
-const  onDelete = async(id) =>{
-  await axios.delete(`/del/${id}`)
-  console.log(id)
-  getList()
-}
-
-// TODO: 编辑功能
-
-const open = ref(null)
-
-const onEdit = (row)=>{
-  open.value.open(row)
-}
-
-
-// pinia 状态管理
-
-import {useCounterStore} from '@/stores/counter.js'
-
-// 实例化对象
-
-const CounterStore = useCounterStore()
-console.log(CounterStore);
 
 </script>
 
 <template>
-  <div class="app">
-    <el-table :data="list">
-      <el-table-column label="ID" prop="id"></el-table-column>
-      <el-table-column label="姓名" prop="name" width="150"></el-table-column>
-      <el-table-column label="籍贯" prop="place"></el-table-column>
-      <el-table-column label="操作" width="150">
-        <template #default="{row}">
-          <el-button type="primary" @click="onEdit(row)" link >编辑</el-button>
-          <el-button type="danger" @click="onDelete(row.id)" link>删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
-  <Edit ref="open" @getList="getList"/>
+   
+  <el-row>
+    <el-col :sm="12" :lg="6">
+      <el-result
+        icon="success"
+        title="Success Tip"
+        sub-title="Please follow the instructions"
+      >
+      </el-result>
+    </el-col>
+  </el-row>
 
-  <el-button @click="CounterStore.increment">{{ CounterStore.count }}</el-button>
+
 </template>
 
 <style scoped>
-.app {
-  width: 980px;
-  margin: 100px auto 0;
-}
+
+
 </style>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import { viteMockServe } from "vite-plugin-mock"
 
+// element-plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,6 +16,14 @@ export default defineConfig({
     viteMockServe({
       mockPath: "mock",
       localEnabled: true,
+    }),
+
+    // element-plus配置
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
     }),
   ],
   resolve: {
